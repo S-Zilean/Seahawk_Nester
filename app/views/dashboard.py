4c79cf82f92a6adff5849fac17914431afdf0e3f
adil <adil.bouzit@ecoles-epsi.net>
2025-02-17 18:20:56 +0100

    .

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,14 +1,24 @@
 from flask import session, redirect, url_for, request, render_template, flash
 from functools import wraps
 from app.db_helper.db_connection import db_connect
+import socket, platform, os
 
 # Définition d'un décorateur pour vérifier si l'utilisateur est connecté
 from app.helper.user_session import login_required
 login_required(any)
 
-
+    
 def init_dashboard(app):
     @app.route('/dashboard')
     @login_required
-    def dashboard():
-        return render_template('dashboard.html')
+
+
+    def dashboard():        
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        fqdn = socket.getfqdn()
+        os_name = platform.system()
+        os_version = platform.version()
+
+
+        return render_template('dashboard.html', ip_address = ip_address, hostname = hostname, fqdn = fqdn, os_name = os_name, os_version = os_version)
