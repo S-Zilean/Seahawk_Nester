58fe2c8c4e8ed5ad0a0c85b1ee2448c00173a56c
adil <adil.bouzit@ecoles-epsi.net>
2025-02-21 19:53:49 +0100

    modification fichier

--- app/db_helper/__init__.py
+++ app/db_helper/__init__.py
@@ -1,7 +1,7 @@
 # Ce fichier initialise le sous-package db_helper.
 
 from .db_connection import db_connect
-from .db_franchise import get_franchise, get_all_franchises
+from .db_database import get_franchise, get_all_franchises
 from .db_table import table, get_all_tables
 from .db_data import get_data, get_table_data, get_specific_data
 from .db_query import do_request
