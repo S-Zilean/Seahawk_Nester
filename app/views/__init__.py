b91a17b2a061448d28c893fe8a4ae05da65f8ca1
adil <adil.bouzit@ecoles-epsi.net>
2025-02-27 16:00:45 +0100

    first commit reboot

--- app/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sondes import init_sondes
-from .authentification import init_authentification
-from .dashboard import init_dashboard
-from .scan_report import init_scan_report
-from .admin_tools import init_admin_tools
