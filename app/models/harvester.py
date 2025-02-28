598994aa2789a15e18050e8bb234cc4dfbae7615
adil <adil.bouzit@ecoles-epsi.net>
2025-02-28 14:02:11 +0100

    ajout du modèle Harvester

---
+++ app/models/harvester.py
@@ -0,0 +1,17 @@
+class Mytest :
+    def __init__(self, value):
+        self.key = {
+            "id" : value[0],
+            "Etat" : value[1],
+            "ip" : value[2],
+            "Hostname" : value[3],
+            "Version" : value[4],
+            "Latency" : value[5],
+            "Machine_count" : value[6]
+        }
+
+    def __str__(self):
+        return str(self.key)
+
+    def __repr__(self):
+        return str(self.key)
