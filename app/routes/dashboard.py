e79919454d794b6d3ead104f8084d78174bc4e75
adil <adil.bouzit@ecoles-epsi.net>
2025-03-05 00:28:05 +0100

    ajout commentaires

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -5,10 +5,4 @@ def init_dashboard(app):
     @app.route('/dashboard')
     @login_required
     def dashboard():
-        # Récupération de toutes les franchises
-        temp_franchises = get_all_franchises()
-
-        # Création de la liste des franchises
-        list_franchises = [franchise for franchise in temp_franchises]
-
         return render_template('dashboard.html')
--- app/routes/franchise.py
+++ app/routes/franchise.py
@@ -4,32 +4,41 @@ from app.fonctions.db_authentification import login_required
 import json
 
 
-
-app = Flask(__name__)
-
+# Définition de la fonction init_franchise qui prend 'app' comme argument
 def init_franchise(app):
+
+    # Décorateur pour définir la route '/<nom_franchise>'
     @app.route('/<nom_franchise>')
+
+    # Décorateur pour exiger que l'utilisateur soit authentifié
     @login_required
+
+    # Définition de la fonction franchise qui prend 'nom_franchise' comme argument
     def franchise(nom_franchise):
-        # Récupération de toutes les franchises
+
+        # Récupération de toutes les franchises disponibles
         all_franchises = get_all_franchises()
 
-        # Vérifier si le nom de la franchise est valide
+        # Vérification si le nom de la franchise est valide en le cherchant dans la liste des franchises
         if nom_franchise in all_franchises:
+
+            # Mise à jour de l'état des harvesters pour la franchise spécifiée
             update_harvester_status(nom_franchise)
-            
+
             try:
+                # Récupération des données de tous les harvesters pour la franchise spécifiée
                 harvesters_data = getall_harvesters_data(nom_franchise)
 
+                # Rendu du template 'franchise.html' avec les données récupérées
                 return render_template('franchise.html', harvester=harvesters_data, franchise_name=nom_franchise)
             except Exception as e:
-                e = {"Hostname" : "Erreur : Aucune donnée trouvée",
-                "ip" : "Erreur : Aucune donnée trouvée",
-                "Etat" : "Erreur : Aucune donnée trouvée"}
-                return render_template('franchise.html', harvester= e,scan_report=e,franchise_name=nom_franchise)
-            
+                # En cas d'erreur, définir un message d'erreur pour les données du harvester
+                e = {"Hostname": "Erreur : Aucune donnée trouvée",
+                     "ip": "Erreur : Aucune donnée trouvée",
+                     "Etat": "Erreur : Aucune donnée trouvée"}
+
+                # Rendu du template 'franchise.html' avec le message d'erreur
+                return render_template('franchise.html', harvester=e, scan_report=e, franchise_name=nom_franchise)
+
         # Si la franchise n'existe pas, retourner une erreur 404
         abort(404, description="Franchise non trouvée")
-
-if __name__ == '__main__':
-    app.run(debug=True)
